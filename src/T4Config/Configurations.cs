a8a0350a857ca4890ea9ef16619e2b15f42429bb
Ryan Anderson <ryan.anderson@buzzuti.com>
2015-02-26 14:16:41 -0600

    formatted output corrections.

--- src/T4Config/Configurations.cs
+++ src/T4Config/Configurations.cs
@@ -3,52 +3,52 @@ using System.Configuration;
 
 namespace T4Config
 {
-    public interface IConfigurations
-    {
-        string Key1 { get; }
-        Guid Key2 { get; }
-        int Key3 { get; }
-    }
+	public interface IConfigurations
+	{
+		string Key1 { get; }
+		Guid Key2 { get; }
+		int Key3 { get; }
+	}
 
-    public class Configurations : IConfigurations
-    {
-        public string Key1
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["Key1"];
-            }
-        }
-        public Guid Key2
-        {
-            get
-            {
-                return new Guid(ConfigurationManager.AppSettings["Key2"]);
-            }
-        }
-        public int Key3
-        {
-            get
-            {
-                return Convert.ToInt32(ConfigurationManager.AppSettings["Key3"]);
-            }
-        }
-    }
+	public class Configurations : IConfigurations
+	{
+		public string Key1 
+		{
+			get 
+			{
+				return ConfigurationManager.AppSettings["Key1"];
+			}
+		}
+		public Guid Key2 
+		{
+			get 
+			{
+				return new Guid(ConfigurationManager.AppSettings["Key2"]);
+			}
+		}
+		public int Key3 
+		{
+			get 
+			{
+				return Convert.ToInt32(ConfigurationManager.AppSettings["Key3"]);
+			}
+		}
+	}
 
-    public interface IConnectionStrings
-    {
-        string LocalSqlServer { get; }
-    }
+	public interface IConnectionStrings
+	{
+		string LocalSqlServer { get; }
+	}
 
-    public class ConnectionStrings : IConnectionStrings
-    {
-        public string LocalSqlServer
-        {
-            get
-            {
-                return ConfigurationManager.ConnectionStrings["LocalSqlServer"].ConnectionString;
-            }
-        }
-    }
+	public class ConnectionStrings : IConnectionStrings
+	{
+		public string LocalSqlServer 
+		{
+			get 
+			{
+				return ConfigurationManager.ConnectionStrings["LocalSqlServer"].ConnectionString;
+			}
+		}
+	}
 }
 
