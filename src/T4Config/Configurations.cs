7b65dc6e16664e89448f834bf05f4597d7907f8e
Ryan Anderson <ryan.anderson@buzzuti.com>
2015-02-26 13:33:21 -0600

    Corrected Issue with the File.Exists check.

--- src/T4Config/Configurations.cs
+++ src/T4Config/Configurations.cs
@@ -2,44 +2,44 @@
 
 namespace T4Config
 {
-    public interface IConfigurations
-    {
-        string Key1 { get; }
-        string Key2 { get; }
-    }
+	public interface IConfigurations
+	{
+			 string Key1 { get; }
+			 string Key2 { get; }
+		}
 
-    public class Configurations : IConfigurations
-    {
-        public string Key1
-        {
-            get
-            {
-                return "value1";
-            }
-        }
-        public string Key2
-        {
-            get
-            {
-                return "value2";
-            }
-        }
-    }
+	public class Configurations : IConfigurations
+	{
+				public string Key1 
+		{
+			get 
+			{
+				return "value1";
+			}
+		}
+			public string Key2 
+		{
+			get 
+			{
+				return "value2";
+			}
+		}
+		}
 
-    public interface IConnectionStrings
-    {
-        string LocalSqlServer { get; }
-    }
+		public interface IConnectionStrings
+	{
+				string LocalSqlServer { get; }
+		}
 
-    public class ConnectionStrings : IConnectionStrings
-    {
-        public string LocalSqlServer
-        {
-            get
-            {
-                return @"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|aspnetdb.mdf;User Instance=true";
-            }
-        }
-    }
-}
+	public class ConnectionStrings : IConnectionStrings
+	{
+				public string LocalSqlServer 
+		{
+			get 
+			{
+				return @"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|aspnetdb.mdf;User Instance=true";
+			}
+		}
+		}
+	}
 
