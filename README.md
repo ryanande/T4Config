5070e655dd690317ea6611d443f1fe69b1d77190
Ryan Anderson <ryan.anderson@buzzuti.com>
2015-02-25 23:41:46 -0600

    Updated the readme to be more thorough. Added configFile variable in the T4 for easier configuration.

--- README.md
+++ README.md
@@ -2,8 +2,49 @@
 
 A down and dirty way to have strongly typed App Settings and Connection Strings using T4.
 
-In a nutshell we create an interface called IConfigurations, so you can do all your fancy DI/IoC stuff, and a concrete implementation called Configurations. (both names totally customizable.
+In a nutshell we create an interface called IConfigurations/ IConnectionStrings, so you can do all your fancy DI/IoC stuff, and a concrete implementation called Configurations/ ConnectionStrings. (both names totally customizable.
 
 The T4 template loops through the web.config or app.config appSettings and generates a read only property for each key and returns the values placed in the settings value.
+
+If you have a configuration section of appSettings which looked like this;
+
+    <appSettings>
+    	<add key="Key1" value="value1" />
+    	<add key="Key2" value="value2" />
+	</appSettings>
+
+After the T4 file compiled you would end up with this;
+
+    public interface IConfigurations
+    {
+        string Key1 { get; }
+        string Key2 { get; }
+    }
+
+    public class Configurations : IConfigurations
+    {
+        public string Key1
+        {
+            get
+            {
+                return "value1";
+            }
+        }
+        public string Key2
+        {
+            get
+            {
+                return "value2";
+            }
+        }
+    }
+
+Same goes for connection strings as well!
+
+## How do I do it?
+- Copy the Configurations.tt file from the src directory to your project.
+- Update the configFile value to the proper config file (web or app).
+- Right click on the file and click "Run Custom Tool".
+
 
 Enjoy!
--- src/T4Config/Configurations.cs
+++ src/T4Config/Configurations.cs
@@ -2,68 +2,44 @@
 
 namespace T4Config
 {
-    public interface IConfigurations
-    {
-        string Key1 { get; }
-        string Key2 { get; }
-        string Key3 { get; }
-        string Key4 { get; }
-        string Key5 { get; }
-    }
+	public interface IConfigurations
+	{
+			string Key1 { get; }
+			string Key2 { get; }
+		}
 
-    public class Configurations : IConfigurations
-    {
-        public string Key1
-        {
-            get
-            {
-                return "value1";
-            }
-        }
-        public string Key2
-        {
-            get
-            {
-                return "value2";
-            }
-        }
-        public string Key3
-        {
-            get
-            {
-                return "value3";
-            }
-        }
-        public string Key4
-        {
-            get
-            {
-                return "value4";
-            }
-        }
-        public string Key5
-        {
-            get
-            {
-                return "value5";
-            }
-        }
-    }
+	public class Configurations : IConfigurations
+	{
+				public string Key1 
+		{
+			get 
+			{
+				return "value1";
+			}
+		}
+			public string Key2 
+		{
+			get 
+			{
+				return "value2";
+			}
+		}
+		}
 
-    public interface IConnectionStrings
-    {
-        string LocalSqlServer { get; }
-    }
+		public interface IConnectionStrings
+	{
+				string LocalSqlServer { get; }
+		}
 
-    public class ConnectionStrings : IConnectionStrings
-    {
-        public string LocalSqlServer
-        {
-            get
-            {
-                return @"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|aspnetdb.mdf;User Instance=true";
-            }
-        }
-    }
-}
+	public class ConnectionStrings : IConnectionStrings
+	{
+				public string LocalSqlServer 
+		{
+			get 
+			{
+				return @"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|aspnetdb.mdf;User Instance=true";
+			}
+		}
+		}
+	}
 
